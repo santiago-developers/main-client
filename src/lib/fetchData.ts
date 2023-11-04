3236da9ab2e50b3c48d7ba35210e406e19031867
DONGUKwillsucceed <rlaehddnr1009@naver.com>
2023-11-04 15:57:48 +0900

    fix: fetchData url을 localhost에서 server로 수정

--- src/lib/fetchData.ts
+++ src/lib/fetchData.ts
@@ -1,15 +1,30 @@
 import { error } from "console";
 
 export async function SantiagoPost<T, R>(url: string, dto: T): Promise<R> {
-	const res = await fetch(`http://localhost:11009/${url}`, {
+	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify(dto),
 	});
-	console.log(res.status);
 	if (!res.ok) {
+		throw new Error(`Failed to fetch posts, received status ${res.status}`);
+	}
+	const data = await res.json();
+	return data;
+}
+
+export async function SantiagoPut<T, R>(url: string, dto: T): Promise<R> {
+	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(dto),
+	});
+
+	if(!res.ok) {
 		throw new Error(`Failed to fetch posts, received status ${res.status}`);
 	}
 	const data = await res.json();
