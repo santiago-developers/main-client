2f53a4064f2f9e1d6d75745c0eb63431b9a9c1fc
Yeeun Lee <maraisienne.lee@gmail.com>
2023-11-21 00:18:52 +0900

    feat: BestList 데이터 연동

--- src/components/magazines/BestList.tsx
+++ src/components/magazines/BestList.tsx
@@ -6,38 +6,61 @@ import BFanaticsSvg from "@public/images/magazines/bFanatics.svg";
 import { SantiagoGet } from "lib/fetchData";
 import { useEffect } from "react";
 import { useState } from "react";
+import writeStore from "store/writeStore";
+import Image from "next/image";
+
+type BestListProps ={
+	id:string;
+	name:string;
+    imageUrl: string | null;
+    region: string[]
+}
 
 const BestList = () => {
+	// const { regionId } = writeStore();
 	const bestList = ["Best Photographers", "Best Writers", "Best Fanatics"];
 
 	const [bWriters, setBWriters] = useState([]);
 	const [bPhotographers, setBPhotographers] = useState([]);
 	const [bFanatics, setBFanatics] = useState([]);
 
-	const fetchData = async () => {
-		const bestList = await SantiagoGet("users");
-		console.log(bestList);
+	const regionId = "0bcbbb91-89bd-48f7-9562-ec662b6fd3a2";
+	const fetchData = async (regionId: string) => {
+		const bWritersList = await SantiagoGet(
+			`users?query_type=photoBest%20&region_id=${regionId}&base=0&limit=5`,
+		);
+		setBWriters(bWritersList.data);
+		const bPhotographersList = await SantiagoGet(
+			`users?query_type=writingBest%20&region_id=${regionId}&base=0&limit=5`,
+		);
+		setBPhotographers(bPhotographersList.data);
+		const bFanaticsList = await SantiagoGet(
+			`users?query_type=fanaticBest%20&region_id=${regionId}&base=0&limit=5`,
+		);
+		setBFanatics(bFanaticsList.data);
 	};
 
-	// useEffect(() => {
-	// 	fetchData();
-	// }, []);
+	useEffect(() => {
+		fetchData(regionId);
+	}, []);
+
+	const fetchDataList = [bWriters, bPhotographers, bFanatics];
 
 	return (
 		<>
 			{bestList.map((item, index) => (
 				<div tw="text-[14px]" key={index}>
-					<div tw="whitespace-nowrap flex gap-4 pb-2 font-bold ">
-						{item === "Best Photographers" && <BPhotographersSvg />}
-						{item === "Best Writers" && <BWritersSvg />}
-						{item === "Best Fanatics" && <BFanaticsSvg />}
+					<div tw="whitespace-nowrap flex gap-4 pb-2 font-bold items-center">
+						{index == 0 && <BPhotographersSvg />}
+						{index == 1 && <BWritersSvg />}
+						{index == 2 && <BFanaticsSvg />}
 						<span>{item}</span>
 					</div>
-					{Array.from({ length: 5 }, (_, index) => (
+					{fetchDataList[index].map((item:BestListProps, index) => (
 						<div tw="flex items-center gap-4 pb-5" key={index}>
 							{index + 1}
-							<DefautUserSvg tw="w-[24.5px] h-[24.5px]" />
-							David
+							<Image src={item.imageUrl || "images/defaultUser.svg"} alt="userImage" width={24.5} height={24.5} />
+							{item.name}
 						</div>
 					))}
 				</div>
