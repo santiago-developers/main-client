5d32e8124e8b26e7549ea940e48db79d31abe44d
Marais <maraisienne.lee@gmail.com>
2024-02-18 17:08:21 +0900

    chore: writeStore에서 imageUrlId 사용하지 않아서 제거

--- src/store/writeStore.ts
+++ src/store/writeStore.ts
@@ -2,21 +2,17 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface WriteStoreProps {
-	imageIds: string[];
 	regionId: string;
-	setImageId: (newIds: string[]) => void;
 	setRegionId: (newRegionId: string) => void;
 }
 
 const writeStore = create(
 	persist<WriteStoreProps>(
 		(set) => ({
-			imageIds: [],
 			regionId: "",
-			setImageId: (newIds) => set({ imageIds: newIds }),
 			setRegionId: (newRegionId) => set({ regionId: newRegionId }),
 		}),
-		{ name: "image" },
+		{ name: "write" },
 	),
 );
 
