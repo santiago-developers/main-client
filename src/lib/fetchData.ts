764314fa8c5521be8f4de73d942d651d0ecb42c5
Marais <maraisienne.lee@gmail.com>
2024-02-18 19:40:11 +0900

    chore: BaseURL 변수 설정

--- src/lib/fetchData.ts
+++ src/lib/fetchData.ts
@@ -1,5 +1,7 @@
+export const BaseURL = "http://3.34.114.67:11009/"
+
 export async function SantiagoPost<T, R>(url: string, dto: T): Promise<R> {
-	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
+	const res = await fetch(`${BaseURL}${url}`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
@@ -20,7 +22,7 @@ export async function SantiagoPostWithAutorization<T, R>(
 	if (!accessToken) {
 		throw new Error("Access token is not available");
 	}
-	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
+	const res = await fetch(`${BaseURL}${url}`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
@@ -39,7 +41,7 @@ export async function SantiagoPostWithAutorization<T, R>(
 
 export async function SantiagoImagePost<R>(dto): Promise<R> {
 	const url = "magazines/upload_image";
-	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
+	const res = await fetch(`${BaseURL}${url}`, {
 		method: "POST",
 		body: dto,
 	});
@@ -51,7 +53,7 @@ export async function SantiagoImagePost<R>(dto): Promise<R> {
 }
 
 export async function SantiagoPut<T, R>(url: string, dto: T): Promise<R> {
-	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
+	const res = await fetch(`${BaseURL}${url}`, {
 		method: "PUT",
 		headers: {
 			"Content-Type": "application/json",
@@ -68,7 +70,7 @@ export async function SantiagoPut<T, R>(url: string, dto: T): Promise<R> {
 
 export async function SantiagoPutWithAutorization<T, R>(url: string, dto: T): Promise<R> {
 	const accessToken = localStorage.getItem("accessToken");
-	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
+	const res = await fetch(`${BaseURL}${url}`, {
 		method: "PUT",
 		headers: {
 			"Content-Type": "application/json",
@@ -86,7 +88,7 @@ export async function SantiagoPutWithAutorization<T, R>(url: string, dto: T): Pr
 export async function SantiagoPostNoRes<T>(url: string, dto?: T) {
 	const accessToken = localStorage.getItem("accessToken");
 	try {
-		const response = await fetch(`http://3.34.114.67:11009/${url}`, {
+		const response = await fetch(`${BaseURL}${url}`, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -105,7 +107,7 @@ export async function SantiagoPostNoRes<T>(url: string, dto?: T) {
 export async function SantiagoDeletetNoRes(url: string) {
 	const accessToken = localStorage.getItem("accessToken");
 	try {
-		const response = await fetch(`http://3.34.114.67:11009/${url}`, {
+		const response = await fetch(`${BaseURL}${url}`, {
 			method: "DELETE",
 			headers: {
 				"Content-Type": "application/json",
@@ -122,7 +124,7 @@ export async function SantiagoDeletetNoRes(url: string) {
 }
 
 export async function SantiagoGet<T>(url: string): Promise<T> {
-	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
+	const res = await fetch(`${BaseURL}${url}`, {
 		method: "GET",
 		headers: {
 			"Content-Type": "application/json",
@@ -142,7 +144,7 @@ export async function SantiagoDelete<T>(url: string): Promise<T> {
 	if (!accessToken) {
 		throw new Error("Access token is not available");
 	}
-	const res = await fetch(`http://3.34.114.67:11009/${url}`, {
+	const res = await fetch(`${BaseURL}${url}`, {
 		method: "DELETE",
 		headers: {
 			"Content-Type": "application/json",
