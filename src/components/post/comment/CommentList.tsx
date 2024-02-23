851d872c0be559433d79408eb3093edac89980e6
DONGUKwillsucceed <rlaehddnr1009@naver.com>
2024-02-23 18:36:14 +0900

    fix: descending order sort

--- src/components/post/comment/CommentList.tsx
+++ src/components/post/comment/CommentList.tsx
@@ -7,11 +7,18 @@ import Comment from "./Comment";
 type CommentListProp = {
 	magazineId: string | undefined;
 	commentList: CommentProps[] | undefined;
-	setCommentList: React.Dispatch<React.SetStateAction<CommentProps[] | undefined>>
-	setCommentCount: React.Dispatch<React.SetStateAction<number>>
+	setCommentList: React.Dispatch<
+		React.SetStateAction<CommentProps[] | undefined>
+	>;
+	setCommentCount: React.Dispatch<React.SetStateAction<number>>;
 };
 
-const CommentList = ({ magazineId, commentList, setCommentList, setCommentCount }: CommentListProp) => {
+const CommentList = ({
+	magazineId,
+	commentList,
+	setCommentList,
+	setCommentCount,
+}: CommentListProp) => {
 	const [selectedCommentIdx, setSelectedCommentIdx] = useState<
 		number | undefined
 	>(undefined);
@@ -22,28 +29,32 @@ const CommentList = ({ magazineId, commentList, setCommentList, setCommentCount 
 				<div
 					tw="relative h-[410px] overflow-y-scroll"
 					className="customScrollbar">
-					{commentList?.sort((a, b)=> Number(b.createdAt) - Number(a.createdAt)).map(
-						(item: CommentProps, index) =>
-							!item.parentId && (
-								<Comment
-									key={item.id}
-									magazineId={magazineId}
-									comment={item}
-									index={index}
-									setSelectedCommentIdx={
-										setSelectedCommentIdx
-									}
-									isSelected={
-										selectedCommentIdx === index
-											? true
-											: false
-									}
-									commentList={commentList}
-									setCommentList={setCommentList}
-									setCommentCount={setCommentCount}
-								/>
-							),
-					)}
+					{commentList
+						?.sort((a, b) => {
+							return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+						})
+						.map(
+							(item: CommentProps, index) =>
+								!item.parentId && (
+									<Comment
+										key={item.id}
+										magazineId={magazineId}
+										comment={item}
+										index={index}
+										setSelectedCommentIdx={
+											setSelectedCommentIdx
+										}
+										isSelected={
+											selectedCommentIdx === index
+												? true
+												: false
+										}
+										commentList={commentList}
+										setCommentList={setCommentList}
+										setCommentCount={setCommentCount}
+									/>
+								),
+						)}
 				</div>
 			</Paper>
 		</>
