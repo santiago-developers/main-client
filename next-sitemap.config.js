7e4fb75bfdfc0f15f1c2f9120504f05412a89963
DONGUKwillsucceed <rlaehddnr1009@naver.com>
2024-03-10 22:21:00 +0900

    fix/url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    siteUrl: 'http://localhost:3000/main',
+    siteUrl: 'http://santiago-story.com/main',
     robotsTxtOptions: {
         // 추가 sitemap 설정
         additionalSitemaps: [
-            'http://localhost:3000/sitemap/product-sitemap.xml', //실제 도메인으로 변경해야함
+            'http://santiago-story.com/sitemap/product-sitemap.xml', //실제 도메인으로 변경해야함
         ]
     } // robots.txt 옵션 설정
   }
