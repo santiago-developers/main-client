0b1a4329c89c60d2635a4012427584a8160fbfeb
Marais <maraisienne.lee@gmail.com>
2024-04-16 18:53:47 +0900

    feat: Suspense로 페이지 로딩 처리

--- src/components/layout/SantiagoLayout.tsx
+++ src/components/layout/SantiagoLayout.tsx
@@ -1,6 +1,8 @@
 import { StrictPropsWithChildren } from "types/global";
 import SantiagoHeader from "./header/SantiagoHeader";
 import SantiagoFooter from "./footer/SantiagoFooter";
+import { Suspense } from "react";
+import Loading from "@pages/loading";
 
 const SantiagoLayout: React.FunctionComponent<StrictPropsWithChildren> = ({
 	children,
@@ -8,7 +10,9 @@ const SantiagoLayout: React.FunctionComponent<StrictPropsWithChildren> = ({
 	return (
 		<>
 			<SantiagoHeader />
-			<div className="wrap">{children}</div>
+			<div className="wrap">
+				<Suspense fallback={<Loading />}>{children}</Suspense>
+			</div>
 			<SantiagoFooter />
 		</>
 	);
---
+++ src/pages/loading.tsx
@@ -0,0 +1,13 @@
+import tw from "twin.macro";
+import React from "react";
+import RingLoader from "react-spinners/RingLoader";
+
+const Loading = () => {
+	return (
+		<div tw="w-full h-screen flex justify-center items-center">
+			<RingLoader color="#05C3B6" size={50} />
+		</div>
+	);
+};
+
+export default Loading;
