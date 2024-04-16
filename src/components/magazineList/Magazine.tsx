45ef4b57cb2892a2f2e266816ef6e815828109d9
Marais <maraisienne.lee@gmail.com>
2024-04-16 19:04:12 +0900

    fix: Magazine 이미지 깨지는 오류 수정
    - cover 옵션 추가

--- src/components/magazineList/Magazine.tsx
+++ src/components/magazineList/Magazine.tsx
@@ -51,6 +51,9 @@ const Magazine = ({ item }: Props) => {
 							}
 							alt="postImage"
 							fill
+							style={{
+								objectFit: "cover",
+							}}
 							sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
 						/>
 					</div>
